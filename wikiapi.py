e793196075826b36c6709074fba9bb45ef7bb148
daxjjk-kafGQB <77869182+DaxJJKelson@users.noreply.github.com>
2025-05-08 20:42:09 -0600

    updated wikiapi

--- wikiapi.py
+++ wikiapi.py
@@ -1,13 +1,18 @@
 import requests
+import wikipedia
 
-def get_wikipedia_extract(title, lang="en"):
-    url = f"https://{lang}.wikipedia.org/api/rest_v1/page/summary/{title}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("extract")
-    else:
-        return f"Error: {response.status_code}"
-
-extract = get_wikipedia_extract("Paris")
-print("Extract:", extract)
+def get_wikipedia_content(title, language="en"):
+    try:
+        wikipedia.set_lang(language)
+        page = wikipedia.page(title)
+        return {
+            "title": page.title,
+            "content": page.content,
+            "url": page.url
+        }
+    except wikipedia.exceptions.DisambiguationError as e:
+        return{"error": f"Disambiguation error: {e.options}"}
+    except wikipedia.exceptions.PageError:
+        return {"error": "Page not found"}
+    except Exception as e:
+        return {"error": str(e)}
